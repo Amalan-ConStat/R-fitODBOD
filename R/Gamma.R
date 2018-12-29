678818743aec16b1d1fee56e71034e90eaa44b1f
Amalan-ConStat <amalan0595@gmail.com>
2018-12-29 20:08:07 +0530

    dGAMMA function added

--- R/Gamma.R
+++ R/Gamma.R
@@ -15,8 +15,8 @@
 #' The probability density function and cumulative density function of a
 #' unit bounded Gamma distribution with random variable P are given by
 #'
-#' \deqn{g_{P}(p) = \frac{c^l p^{c-1}}{g(l)} [ln(1/p)]^{l-1} } ;    \eqn{0 \le p \le 1}
-#' \deqn{G_{P}(p) = \int_0^p \frac{c^l p^{c-1}}{g(l)} [ln(1/p)]^{l-1} d(p)} ; \eqn{0 \le p \le 1}
+#' \deqn{g_{P}(p) = \frac{ c^l p^{c-1}}{\gamma(l)} [ln(1/p)]^{l-1} } ;    \eqn{0 \le p \le 1}
+#' \deqn{G_{P}(p) = \frac{ Ig(l,cln(1/p))}{\gamma(l) } ; \eqn{0 \le p \le 1}
 #' \deqn{l,c > 0}
 #'
 #' The mean the variance are denoted by
@@ -27,7 +27,8 @@
 #' \deqn{E[P^r]=(\frac{c}{c+r})^l }
 #' \eqn{r = 1,2,3,...}
 #'
-#' Defined as \eqn{g(l)=\int }
+#' Defined as \eqn{\gamma(l)= } is the gamma function
+#' Defined as \eqn{Ig(l,cln(1/p))= \int_0^{cln(1/p)} t^{l-1} e^{-t}dt } is the Lower incomplete gamma function
 #'
 #'
 #' \strong{NOTE} : If input parameters are not in given domain  conditions necessary error
@@ -50,6 +51,8 @@
 #'
 #'  @seealso
 #'  \code{\link[stats]{dgamma}}
+#'
+#'  \code{\link[base]{gamma}}
 #'
 #' @examples
 #' plotting the random variables and probability values
@@ -86,6 +89,114 @@
 #' @export
 dGAMMA<-function(p,c,l)
 {
+  #checking if inputs consist NA(not assigned)values, infinite values or NAN(not a number)values
+  #if so creating an error message as well as stopping the function progress.
+  if(any(is.na(c(p,c,l))) | any(is.infinite(c(p,c,l))) | any(is.nan(c(p,c,l))) )
+  {
+    stop("NA or Infinite or NAN values in the Input")
+  }
+  else
+  {
+    #checking if shape parameters are greater than zero, if not providing error message and
+    #stopping the function progress
+    if(c <= 0 | l <= 0)
+    {
+      stop("Shape parameters cannot be less than or equal to zero")
+    }
+    else
+    {
+      ans<-NULL
+      #for each input values in the vector necessary calculations and conditions are applied
+      for (i in 1:length(p))
+      {
+        if(p[i]<0 |p[i]>1)
+        {
+          stop("Invalid values in the input")
+        }
+        else
+        {
+          ans[i]<-(c^l* p[i]^(c-1)*(log(1/p[i]))^(l-1))/gamma(l)
+        }
+      }
+    }
+    mean<-(c/(c+1))^l             #according to theory the mean value
+    variance<-(c/(c+2))^l - (c/(c+1))^(2*l)     #according to theory the variance value
+    # generating an output in list format consisting pdf,mean and variance
+    output<-list("pdf"=ans,"mean"=mean,"var"=variance)
+    return(output)
+  }
+}
 
+#' @rdname dGAMMA
+#' @export
+pGAMMA<-function(p,c,l)
+{
+
+}
+
+#' @rdname dGAMMA
+#' @export
+mazGAMMA<-function(r,c,l)
+{
+
+}
+
+#' @export
+dGamma1Bin<-function()
+{
+
+}
+
+#' @export
+pGamma1Bin<-function()
+{
+
+}
+
+#' @export
+NegLLGamma1Bin<-function()
+{
+
+}
+
+#' @export
+EstMLEGamma1Bin<-function()
+{
+
+}
+
+#' @export
+fitGamma1Bin<-function()
+{
+
+}
+
+#' @export
+dGamma2Bin<-function()
+{
+
+}
+
+#' @export
+pGamma2Bin<-function()
+{
+
+}
+
+#' @export
+NegLLGamma2Bin<-function()
+{
+
+}
+
+#' @export
+EstMLEGamma2Bin<-function()
+{
+
+}
+
+#' @export
+fitGamma2Bin<-function()
+{
 
 }
